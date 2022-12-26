8d353543be81eb74fdec986e6ca6d8a4d79fa4e6
Леонид <manatov2000@yandex.ru>
2022-12-26 18:17:23 +0300

    Очистил ветку

--- Modificators/IDiceRollModificator.h
+++
@@ -1,8 +0,0 @@
-#ifndef LAB4DICE_IDICEROLLMODIFICATOR_H
-#define LAB4DICE_IDICEROLLMODIFICATOR_H
-class IDiceRollModificator {
-public:
-    virtual int Modifiy(int rollResult) const = 0;
-};
-
-#endif //LAB4DICE_IDICEROLLMODIFICATOR_H
